bea51a974f9b8fa5854ab0e7c5fbbb725a6ab2b1
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 14:16:36 +0100

    one positive number

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
@@ -1,8 +1,8 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
 fun parse(s: String) = when (s) {
-    "+1" -> arrayOf(1)
-    else -> emptyArray<Int>()
+    "+1" -> arrayOf(s.toInt())
+    else -> emptyArray()
 }
 
 fun shiftFrequency(vararg shifts: Int) = 0 + shifts.sum()
