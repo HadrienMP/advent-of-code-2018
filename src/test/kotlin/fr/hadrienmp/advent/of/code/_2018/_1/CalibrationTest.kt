6818c56703ad5d94313fccda8d7be96c338f6614
HadrienMP <public@hadrienmp.fr>
2018-12-05 19:29:08 +0200

    should return the first frequency that was reached twice

---
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/CalibrationTest.kt
@@ -0,0 +1,10 @@
+package fr.hadrienmp.advent.of.code._2018._1
+
+import org.junit.jupiter.api.Test
+
+class CalibrationTest {
+    @Test
+    internal fun should_return_the_first_frequency_that_was_reached_twice() {
+
+    }
+}
