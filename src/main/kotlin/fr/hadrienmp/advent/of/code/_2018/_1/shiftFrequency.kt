702ec6f33f1eb79d2c79d85827dbaa1d602e4e35
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 14:14:24 +0100

    should return an empty array for an empty string

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
@@ -1,4 +1,7 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
-fun parse(s: String) = emptyArray<Int>()
+fun parse(s: String) = when (s) {
+    else -> emptyArray<Int>()
+}
+
 fun shiftFrequency(vararg shifts: Int) = 0 + shifts.sum()
