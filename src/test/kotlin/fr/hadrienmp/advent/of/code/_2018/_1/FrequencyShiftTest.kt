bea5a67df091f74b25609517d292bcd1cb5cd9ab
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 13:24:10 +0100

    at_first_the_frequency_should_be_0

--- src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftTest.kt
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftTest.kt
@@ -1,10 +1,11 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class FrequencyShiftTest {
     @Test
     internal fun at_first_the_frequency_should_be_0() {
-
+        assertEquals(0, 0)
     }
 }
