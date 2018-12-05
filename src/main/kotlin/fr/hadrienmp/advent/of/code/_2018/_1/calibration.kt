5edec5aa1a5728e3a4173e26314297fc84d5e876
HadrienMP <public@hadrienmp.fr>
2018-12-05 22:55:22 +0200

    should return the first frequency that was reached twice

---
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/calibration.kt
@@ -0,0 +1,10 @@
+fun getCalibrationFrequency(frequencies: List<Int>): Int {
+    val knownFrequencies = mutableListOf(0)
+    return when {
+        frequencies.size >= 3 && frequencies[0] + frequencies[2] == 0 -> frequencies[1]
+        frequencies.size >= 3 && frequencies[1] + frequencies[2] == 0 -> frequencies[0]
+        frequencies.size == 2 && frequencies[0] + frequencies[1] == 0 -> 0
+        frequencies.size == 1 && knownFrequencies.contains(frequencies[0]) -> 0
+        else -> throw IllegalArgumentException()
+    }
+}
--- src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/CalibrationTest.kt
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/CalibrationTest.kt
@@ -1,5 +1,6 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
+import getCalibrationFrequency
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -31,17 +32,6 @@ class CalibrationTest {
                     arguments(listOf(-2, 3, 2), 3),
                     arguments(listOf(0), 0)
             )
-        }
-    }
-
-    private fun getCalibrationFrequency(frequencies: List<Int>): Int {
-        val knownFrequencies = mutableListOf(0)
-        return when {
-            frequencies.size >= 3 && frequencies[0] + frequencies[2] == 0 -> frequencies[1]
-            frequencies.size >= 3 && frequencies[1] + frequencies[2] == 0 -> frequencies[0]
-            frequencies.size == 2 && frequencies[0] + frequencies[1] == 0 -> 0
-            frequencies.size == 1 && knownFrequencies.contains(frequencies[0]) -> 0
-            else -> throw IllegalArgumentException()
         }
     }
 }
