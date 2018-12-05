a4f4ce14bb593448a3dfb14a04e5c71e418f916d
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 13:52:50 +0100

    changing the frequency positively once should increase it by the change

---
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftParserTest.kt
@@ -0,0 +1,10 @@
+package fr.hadrienmp.advent.of.code._2018._1
+
+import org.junit.jupiter.api.Test
+
+class FrequencyShiftParserTest {
+    @Test
+    internal fun `should return an empty array for an empty string`() {
+
+    }
+}
