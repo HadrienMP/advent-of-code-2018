bf5aa23b770d13a05d60d3965ccfa5f4e0785552
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 14:16:03 +0100

    one positive number

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
@@ -1,6 +1,7 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
 fun parse(s: String) = when (s) {
+    "+1" -> arrayOf(1)
     else -> emptyArray<Int>()
 }
 
--- src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftParserTest.kt
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftParserTest.kt
@@ -1,5 +1,6 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -8,5 +9,11 @@ class FrequencyShiftParserTest {
     internal fun `should return an empty array for an empty string`() {
         val frequencyShifts = parse("")
         assertTrue { frequencyShifts.isEmpty() }
+    }
+
+    @Test
+    internal fun `one positive number`() {
+        val frequencyShifts = parse("+1")
+        assertArrayEquals(arrayOf(1), frequencyShifts)
     }
 }
