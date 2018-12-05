b52e63cfc0bb8c35a0ebb1a99bef179e4eb2a94b
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 13:50:37 +0100

    changing the frequency positively once should increase it by the change

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
@@ -2,4 +2,4 @@ package fr.hadrienmp.advent.of.code._2018._1
 
 fun shiftFrequency() = 0
 fun shiftFrequency(shift: Int) = shiftFrequency() + shift
-fun shiftFrequency(shift: Int, i: Int) = 0 + shift + i
+fun shiftFrequency(vararg shifts: Int) = 0 + shifts.sum()
