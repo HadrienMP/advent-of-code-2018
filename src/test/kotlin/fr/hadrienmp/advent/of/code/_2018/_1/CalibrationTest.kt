8ba30033e1ede736943f9f3136d368430abe916d
HadrienMP <public@hadrienmp.fr>
2018-12-05 19:43:01 +0200

    should return the first frequency that was reached twice

--- src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/CalibrationTest.kt
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/CalibrationTest.kt
@@ -1,10 +1,13 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CalibrationTest {
     @Test
     internal fun `should return the first frequency that was reached twice`() {
-
+        val frequencies = listOf(2, -1)
+        val calibrationFrequency = 0 + 2 -1
+        assertThat(calibrationFrequency).isEqualTo(1)
     }
 }
