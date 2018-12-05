01cb93dd697ae154ae6a5225f09395e9f6a4519d
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 13:54:17 +0100

    should return an empty array for an empty string

--- src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftParserTest.kt
+++ src/test/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/FrequencyShiftParserTest.kt
@@ -1,10 +1,11 @@
 package fr.hadrienmp.advent.of.code._2018._1
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class FrequencyShiftParserTest {
     @Test
     internal fun `should return an empty array for an empty string`() {
-
+        assertTrue { emptyArray<Int>().isEmpty() }
     }
 }
