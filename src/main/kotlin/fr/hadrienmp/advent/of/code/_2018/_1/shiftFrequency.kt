fa14748f432e3bdfc0eab20ce1049ffe7f25bae7
Hadrien Mens-Pellen <hmens@financeactive.com>
2018-12-05 14:21:41 +0100

    multiple numbers

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/shiftFrequency.kt
@@ -3,7 +3,7 @@ package fr.hadrienmp.advent.of.code._2018._1
 fun parse(s: String): Array<Int> = when (s) {
     "" -> emptyArray()
     "-1, 2, -3, 4, 5" -> arrayOf(-1, 2, -3, 4, 5)
-    else -> arrayOf(s).map(String::toInt).toTypedArray()
+    else -> s.split(", ").map(String::toInt).toTypedArray()
 }
 
 fun shiftFrequency(vararg shifts: Int) = 0 + shifts.sum()
