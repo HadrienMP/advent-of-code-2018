6455c43f0804a17465d69ec176304b1580d951ff
HadrienMP <public@hadrienmp.fr>
2018-12-06 09:52:55 +0200

    should return the first frequency that was reached twice

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/calibration.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/calibration.kt
@@ -1,5 +1,11 @@
 fun getCalibrationFrequency(frequencies: List<Int>,
                             knownFrequencies: MutableList<Int> = mutableListOf(0)): Int {
+    if (frequencies.isEmpty()) {
+        throw IllegalArgumentException()
+    }
+    if (knownFrequencies.contains(frequencies[0])) return frequencies[0]
+
+
     if (frequencies == listOf(-2, 3, 0)) return 1
     else if (frequencies.isNotEmpty() && knownFrequencies.contains(frequencies[0])) return frequencies[0]
     else if (frequencies.size >= 2 && frequencies[0] + frequencies[1] == 0) return knownFrequencies[0]
