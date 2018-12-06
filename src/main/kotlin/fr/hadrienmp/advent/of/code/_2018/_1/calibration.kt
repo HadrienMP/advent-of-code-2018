928b7b33cfc081fc08505515e2f9576da45b2f8b
HadrienMP <public@hadrienmp.fr>
2018-12-06 21:40:33 +0200

    should return the first frequency that was reached twice

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/calibration.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/calibration.kt
@@ -1,16 +1,21 @@
 fun getCalibrationFrequency(frequencies: List<Int>): Int {
-    val knownFrequencies: MutableList<Int> = mutableListOf(0)
-    return frequencies.stream()
-            .map { knownFrequencies.last() + it }
-            .map {
-                if (knownFrequencies.contains(it)) it
-                else {
-                    knownFrequencies.add(it)
-                    null
-                }
-            }
-            .filter { it != null }
-            .map { it!! }
-            .findFirst()
-            .orElseThrow { IllegalArgumentException() }
+    return Device().calibrate(frequencies)
+}
+
+class Device(val knownFrequencies: MutableList<Int> = mutableListOf(0)) {
+    fun calibrate(frequencies: List<Int>): Int {
+        return frequencies.stream()
+                .map { knownFrequencies.last() + it }
+                .map(this::calibrationFrequency)
+                .filter { it != null }
+                .map { it!! }
+                .findFirst()
+                .orElseThrow { IllegalArgumentException() }
+    }
+
+    private fun calibrationFrequency(frequency: Int): Int? {
+        if (knownFrequencies.contains(frequency)) return frequency
+        knownFrequencies.add(frequency)
+        return null
+    }
 }
