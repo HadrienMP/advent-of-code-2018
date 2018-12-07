941a64f4df46d98b7e0befcae847abd4abf01a0e
HadrienMP <public@hadrienmp.fr>
2018-12-07 10:17:06 +0200

    Timing the calibration

--- src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/main.kt
+++ src/main/kotlin/fr/hadrienmp/advent/of/code/_2018/_1/main.kt
@@ -3,9 +3,18 @@ package fr.hadrienmp.advent.of.code._2018._1
 import calibrate
 
 fun main(args: Array<String>) {
-    val frequencies = parse(read("fr/hadrienmp/advent/of/code/_2018/_1/input.txt"))
+    val frequencies = frequencies()
+
     println(frequencies.sum())
-    val timedResult = time { calibrate(frequencies) }
+
+    display(time { calibrate(frequencies) })
+}
+
+private fun frequencies(): List<Int> {
+    return parse(read("fr/hadrienmp/advent/of/code/_2018/_1/input.txt"))
+}
+
+private fun display(timedResult: TimedExecution<Int>) {
     println("Calibration frequency : ${timedResult.result}")
     println("Calibration took ${timedResult.seconds}s")
 }
